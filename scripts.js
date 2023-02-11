b4f7d673e609f41e185a8af14eb35b60b9dc7a9e
Miguel Olarte <lmolarteb@unadvirtual.edu.co>
2023-02-10 19:38:47 -0500

    Se agregan textos con efectos en scroll

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 const logo = document.querySelector('.logo-header');
+const cotizacionTexto = document.querySelector('.cotizacion-texto');
+const cotizacion = document.querySelector('.cotizacion');
+const camisaPolo = document.querySelector('.camisa-polo');
+const textoPolo = document.querySelector('.texto-polo');
 
 window.addEventListener("scroll", function() {
-    var rect = logo.getBoundingClientRect();
+    let rect = logo.getBoundingClientRect();
     if (rect.top < 0) {
       logo.style.filter = 'blur(1px)';
     } else {
@@ -9,4 +13,25 @@ window.addEventListener("scroll", function() {
     }
   });
 
+
+  window.addEventListener("scroll", function(){
+    let rect = cotizacion.getBoundingClientRect();
+    if(rect.top < 0){
+        cotizacionTexto.innerText = 'Cotización';
+    }else{
+        cotizacionTexto.innerText = 'Bienvenido';
+    }
+  })
+
+  window.addEventListener("scroll", function(){
+    let rect = camisaPolo.getBoundingClientRect();
+    if(rect.top < 50){
+        textoPolo.innerText = 'Polo';
+    }else if(rect.top < 140){
+        textoPolo.innerText = 'Tipo';
+    }else{
+        textoPolo.innerText = 'Camisa';
+        
+    }
+  })
 
